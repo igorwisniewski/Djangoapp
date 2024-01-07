752b5a0dea037dc761af5eaf402c3668b5781a21
WisDev <71902497+igorwisniewski@users.noreply.github.com>
2024-01-07 16:37:36 +0100

    update

--- users/admin.py
+++ users/admin.py
@@ -2,17 +2,7 @@ from django.contrib import admin
 from .models import User
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-class CustomerUserAdmin(UserAdmin):
-    fieldsets = (
-        *UserAdmin.fieldsets,
-        (
-            'Custom Field Heading',
-            {
-                'fields':(
-                    'is_customer',
-                    'is_engineer'
-                )
-            }
-        )
-    )
-admin.site.register(User, CustomerUserAdmin)
+# Unregister the provided model admin
+
+class CustomUserAdmin(UserAdmin):
+    pass
